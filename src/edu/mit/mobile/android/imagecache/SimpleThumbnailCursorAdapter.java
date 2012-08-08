548a215e9f9176565f2723edfe3eeeaa3bba2771
Steve Pomeroy <steve@staticfree.info>
2012-08-08 17:41:19 -0400

    Updates LGPL boilerplate on class files

--- src/edu/mit/mobile/android/imagecache/SimpleThumbnailCursorAdapter.java
+++ src/edu/mit/mobile/android/imagecache/SimpleThumbnailCursorAdapter.java
@@ -1,6 +1,7 @@
 package edu.mit.mobile.android.imagecache;
+
 /*
- * Copyright (C) 2011 MIT Mobile Experience Lab
+ * Copyright (C) 2011-2012 MIT Mobile Experience Lab
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
