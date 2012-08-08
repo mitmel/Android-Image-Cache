528b91e2022878ef9e5a058af4fec86e074cc94d
Steve Pomeroy <steve@staticfree.info>
2012-08-08 16:52:28 -0400

    Moves from SoftReferences to LruCache
    This should help improve memory performance, as LruCache is much more
    suited to this task. This additionally raises the memory limit for the
    cache as well as makes the memory cache size configurable.

---
+++ src/edu/mit/mobile/android/imagecache/DrawableMemCache.java
@@ -0,0 +1,27 @@
+package edu.mit.mobile.android.imagecache;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.util.LruCache;
+
+public class DrawableMemCache<T> extends LruCache<T, Drawable> {
+
+	private static final String TAG = DrawableMemCache.class.getSimpleName();
+
+	public DrawableMemCache(int maxSize) {
+		super(maxSize);
+	}
+
+	@Override
+	protected int sizeOf(T key, Drawable value) {
+		int size = 0;
+		if (value instanceof BitmapDrawable) {
+			final Bitmap b = ((BitmapDrawable) value).getBitmap();
+			if (b != null) {
+				size = b.getRowBytes() * b.getHeight();
+			}
+		}
+		return size;
+	}
+}
